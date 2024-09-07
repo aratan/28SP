938e2c7cea3d2efea0babafa5bbb321a5cba0c4e
victor <victor.arbiol@gmail.com>
2024-09-07 18:42:19 +0200

    update

--- web.go
+++ web.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
 func main() {
-    // Ruta para servir archivos estáticos
-    fs := http.FileServer(http.Dir("./web"))
-    
-    // Registrar el servidor de archivos estáticos
-    http.Handle("/", fs)
-    
-    // Iniciar el servidor en el puerto 80
-    log.Println("Servidor web ejecutándose en http://localhost:80")
-    
-    if err := http.ListenAndServe(":80", nil); err != nil {
-        log.Fatalf("Error al iniciar el servidor: %v", err)
-    }
+	// Ruta para servir archivos estáticos
+	fs := http.FileServer(http.Dir("./web"))
+
+	// Registrar el servidor de archivos estáticos
+	http.Handle("/", fs)
+
+	// Iniciar el servidor en el puerto 80
+	log.Println("Servidor web ejecutándose en http://localhost:8081")
+
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatalf("Error al iniciar el servidor: %v", err)
+	}
 }
